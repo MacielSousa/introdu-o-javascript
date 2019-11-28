92c56185342ab43a6934ce23dbec088cf94fdce6
Macielsousa <fmacielsousa@gmail.com>
2019-11-27 21:30:28 -0300

    //Basico2 - Eventos-Class date

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-//Basico2 - Eventos(window X document)
-/**
- * Diferença entre window e document, window é minha janela agora document e o site!
- * window temos acesso ao historico do usuario, dá refresh na janela
- * mudar propriedade da janela por exemplo abrir uma nova janela
- */
-window.addEventListener('focus', event => {
-    console.log("focus");
-});
-
-document.addEventListener('click', event => {
-    console.log("click");
-});
+//Basico2 - Eventos-Class date
+let agora = new Date();
+console.log(agora.toLocaleDateString("pt-BR"));
